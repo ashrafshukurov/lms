3d2394247c59d081bcada33d948b842ae90c9e11
Mehman Osmnaov <mehmanosmanof@gmail.com>
2023-08-11 18:24:45 +0400

    changes

--- src/main/java/az/lms/repository/OrderRepository.java
+++ src/main/java/az/lms/repository/OrderRepository.java
@@ -1,8 +1,13 @@
 package az.lms.repository;
 
+import az.lms.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author Mehman Osmanov on 11.08.23
  * @project LMS
  */
-public interface OrderRepository {
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
