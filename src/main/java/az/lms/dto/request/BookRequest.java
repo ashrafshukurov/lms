d398a8bc8e0f07673a5e53f5f77d150975c3e09c
Ashraf <esrefsukurov7@gmail.com>
2023-08-11 13:53:56 +0400

    Book is changed

--- src/main/java/az/lms/dto/request/BookRequest.java
+++ src/main/java/az/lms/dto/request/BookRequest.java
@@ -1,7 +1,10 @@
 package az.lms.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 /**
  * @author ashraf
@@ -9,10 +12,14 @@ import lombok.Setter;
  */
 @Setter
 @Getter
+@Builder
 public class BookRequest {
     private Long id;
     private String name;
     private String isbn;
     private int count;
     private String image;
+    private String authorName;
+    private LocalDate publishedTime;
+
 }
