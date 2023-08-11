7d01e191c84831b9791ce2a65fe04d3c3062214b
Mehman Osmnaov <mehmanosmanof@gmail.com>
2023-08-11 18:23:27 +0400

    changes

--- src/main/java/az/lms/controller/OrderController.java
+++ src/main/java/az/lms/controller/OrderController.java
@@ -1,9 +1,15 @@
 package az.lms.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Mehman Osmanov on 11.08.23
  * @project LMS
  */
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
 }
