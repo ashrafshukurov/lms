2b5d5d98b25b5d8e7dc996426c9ca7960d572315
Ashraf <esrefsukurov7@gmail.com>
2023-08-12 13:37:54 +0400

    service impl

--- src/main/java/az/lms/service/impl/BookServiceImpl.java
+++ src/main/java/az/lms/service/impl/BookServiceImpl.java
@@ -3,6 +3,7 @@ package az.lms.service.impl;
 import az.lms.dto.request.BookRequest;
 import az.lms.dto.response.BookResponse;
 
+import az.lms.exception.AlreadyExistsException;
 import az.lms.exception.NotFoundException;
 import az.lms.mapper.BookMapper;
 import az.lms.model.Book;
@@ -26,30 +27,59 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void createBook(BookRequest bookRequest) {
-       Book book=bookMapper.requestToEntity(bookRequest);
-       bookRepository.save(book);
+        Book book = bookMapper.requestToEntity(bookRequest);
+        if (bookRepository.existsByIsbn(book.getIsbn())) {
+            throw new AlreadyExistsException("Book with ISBN " + book.getIsbn() + " already exists");
+
+        }
+        bookRepository.save(book);
     }
 
     @Override
     public List<BookResponse> getAllBooks() {
-       List<Book> books= bookRepository.findAll();
-       return books.stream().map(bookMapper::entityToResponse).toList();
+        List<Book> books = bookRepository.findAll();
+        return books.stream().map(bookMapper::entityToResponse).toList();
     }
 
     @Override
-    public void deleteBook(Long id) {
+    public void deleteBook(Long id) throws NotFoundException {
+        try {
+            Book book = bookRepository.findById(id)
+                    .orElseThrow(() -> new NotFoundException("Book with ID " + id + " not found"));
 
+            bookRepository.delete(book);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete book with ID " + id, e);
+        }
     }
+
 
     @Override
-    public BookResponse getBookById(Long id) {
-       Book  book=bookRepository.findById(id).orElseThrow(()-> new NotFoundException("invalid book"));
-       return bookMapper.entityToResponse(book);
+    public BookResponse getBookById(Long id) throws NotFoundException {
+        try {
+            Book book = bookRepository.findById(id)
+                    .orElseThrow(() -> new NotFoundException("Book with ID " + id + " not found"));
+
+            return bookMapper.entityToResponse(book);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to fetch book with ID " + id, e);
+        }
     }
+
 
     @Override
     public void updateBook(Long id, BookRequest bookRequest) {
-
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Book with ID " + id + " not found"));
+        book = Book.builder()
+                .authorName(bookRequest.getAuthorName())
+                .count(bookRequest.getCount())
+                .name(bookRequest.getName())
+                .publishedTime(bookRequest.getPublishedTime())
+                .isbn(bookRequest.getIsbn())
+                .image(bookRequest.getImage()).build();
+       bookRepository
+               .save(book);
     }
 
 }
