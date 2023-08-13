b1250a9b379e2277720ebbcec2a129b051b9f349
cahaciyev <cahaciyev@gmail.com>
2023-08-13 12:13:06 +0400

    Already Exception

--- src/main/java/az/lms/exception/AlreadyExistsException.java
+++ src/main/java/az/lms/exception/AlreadyExistsException.java
@@ -12,11 +12,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public class AlreadyExistsException extends RuntimeException {
     private final HttpStatus status = HttpStatus.CONFLICT;
-<<<<<<< HEAD:src/main/java/az/lms/exception/AlreadyExistsException.java
     private final String message;
-=======
-    public StudentAlreadyExistsException(String message) {
-        super(message);
-    }
->>>>>>> 0a2bbcc (Author updated):src/main/java/az/lms/exception/StudentAlreadyExistsException.java
 }
+
