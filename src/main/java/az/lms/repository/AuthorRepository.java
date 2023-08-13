ab9fac788c93ae2f6a62bc995c057448645843c2
cahaciyev <cahaciyev@gmail.com>
2023-08-13 11:31:13 +0400

    Exception name chanced

---
+++ src/main/java/az/lms/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package az.lms.repository;
+
+import az.lms.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
---
+++ src/main/java/az/lms/service/AuthorService.java
@@ -0,0 +1,29 @@
+/*
+ *Created by Jaweed.Hajiyev
+ *Date:12.08.23
+ *TIME:01:22
+ *Project name:LMS
+ */
+
+package az.lms.service;
+
+import az.lms.dto.request.AuthorRequest;
+import az.lms.dto.request.BookRequest;
+import az.lms.dto.response.AuthorResponse;
+import az.lms.dto.response.BookResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface AuthorService {
+    void createAuthor(AuthorRequest request);
+
+    List<AuthorResponse> getAllAuthors();
+
+    void deleteAuthor(Long id);
+
+    AuthorResponse getAuthorById(Long id);
+
+    void updateAuthors(Long id, AuthorRequest request);
+}
