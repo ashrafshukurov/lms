d7b53a318996521c4c91303af238f989fad64a9d
Mehman Osmnaov <mehmanosmanof@gmail.com>
2023-08-16 18:03:44 +0400

    Validation message added

--- src/main/java/az/lms/dto/request/OrderRequest.java
+++ src/main/java/az/lms/dto/request/OrderRequest.java
@@ -3,7 +3,7 @@ package az.lms.dto.request;
 import az.lms.model.OrderType;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author Mehman Osmanov on 11.08.23
@@ -11,7 +11,10 @@ import java.time.LocalDateTime;
  */
 @Data
 public class OrderRequest {
+   @NotBlank(message = "Student ID cannot be empty")
    private Long studentId;
+   @NotBlank(message = "Book ID cannot be empty")
    private Long bookId;
+   @NotBlank(message = "Order type cannot be empty")
    private OrderType orderType;
 }
