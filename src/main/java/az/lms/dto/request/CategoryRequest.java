b12c62b482526581c5693d4961793ff11fb3db54
cahaciyev <cahaciyev@gmail.com>
2023-08-17 15:14:41 +0400

    Swagger configured

--- src/main/java/az/lms/dto/request/CategoryRequest.java
+++ src/main/java/az/lms/dto/request/CategoryRequest.java
@@ -8,6 +8,7 @@
 package az.lms.dto.request;
 
 import az.lms.model.Book;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -23,9 +24,11 @@ public class CategoryRequest {
 
     @NotBlank(message = "Invalid Name: Empty name")
     @Size(max = 25, min = 2,message = "Invalid Name: Must be of 2 - 255 characters")
+    @ApiModelProperty(name = "Category name",required = true,notes = "Category must be unique")
     private String name;
 
     @NotBlank(message = "Invalid Name: Empty description")
-    @Size(max = 255, min = 5,message = "Invalid Name: Must be of 5 - 255 characters")
+    @Size(max = 255, min = 5, message = "Invalid Name: Must be of 5 - 255 characters")
+    @ApiModelProperty(name = "Category description")
     private String description;
 }
