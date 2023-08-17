4b54a55fc1da4a3cf054962b70d5df17d8bc6128
Ashraf <esrefsukurov7@gmail.com>
2023-08-17 23:30:21 +0400

    Swagger added for Student and Book

--- src/main/java/az/lms/controller/StudentController.java
+++ src/main/java/az/lms/controller/StudentController.java
@@ -1,9 +1,13 @@
 package az.lms.controller;
 
 import az.lms.dto.request.StudentRequest;
+import az.lms.dto.response.BookResponse;
 import az.lms.dto.response.OrderResponse;
 import az.lms.dto.response.StudentResponse;
 import az.lms.service.StudentService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,35 +23,64 @@ import java.util.List;
 @RequestMapping("/v1/student")
 @RequiredArgsConstructor
 public class StudentController {
-   private final StudentService studentService;
+    private final StudentService studentService;
 
-   @PostMapping("/add")
-   public void addStudent(@Valid @RequestBody StudentRequest studentRequest) {
-      studentService.create(studentRequest);
-   }
+    @ApiOperation(value = "adding student", notes = "add to student")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully work"),
+            @ApiResponse(code = 400, message = "Invalid insert")
+    })
+    @PostMapping("/add")
+    public void addStudent(@Valid @RequestBody StudentRequest studentRequest) {
+        studentService.create(studentRequest);
+    }
 
-   @GetMapping("/{fin}")
-   public ResponseEntity<StudentResponse> getStudentById(@PathVariable String fin) {
-      return ResponseEntity.ok(studentService.getById(fin));
-   }
+    @ApiOperation(value = "Get-Student-By-fin", notes = "When you enter fin it will get Student", response = StudentResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully work"),
+            @ApiResponse(code = 404, message = "Invalid getting Student by fin")
+    })
+    @GetMapping("/getStudent/{fin}")
+    public ResponseEntity<StudentResponse> getStudentById(@PathVariable String fin) {
+        return ResponseEntity.ok(studentService.getById(fin));
+    }
 
-   @PutMapping("/update")
-   public void updateStudent(@Valid @RequestBody StudentRequest studentRequest) {
-      studentService.update(studentRequest);
-   }
+    @ApiOperation(value = "Update Student", notes = "Update Student based on fin")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully work"),
+            @ApiResponse(code = 404, message = "Invalid update")
+    })
+    @PutMapping("/update")
+    public void updateStudent(@Valid @RequestBody StudentRequest studentRequest) {
+        studentService.update(studentRequest);
+    }
 
-   @GetMapping("/all")
-   public ResponseEntity<List<StudentResponse>> getAllStudents() {
-      return ResponseEntity.ok(studentService.getAll());
-   }
+    @ApiOperation(value = "Getting-All-Students", notes = "It Will return Student list", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully work"),
+            @ApiResponse(code = 404, message = "Invalid getting Students")
+    })
+    @GetMapping("/all")
+    public ResponseEntity<List<StudentResponse>> getAllStudents() {
+        return ResponseEntity.ok(studentService.getAll());
+    }
 
-   @DeleteMapping("/delete/{fin}")
-   public void deleteStudentByFin(@PathVariable String fin) {
-      studentService.deleteById(fin);
-   }
-
-   @GetMapping("/orders/{fin}")
-   public ResponseEntity<List<OrderResponse>> getStudentOrders(@PathVariable String fin) {
-      return ResponseEntity.ok(studentService.getStudentOrders(fin));
-   }
+    @ApiOperation(value = "Deleting Student", notes = "Deleting Student based on Fin")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully work"),
+            @ApiResponse(code = 404, message = "Invalid deleting student by fin")
+    })
+    @DeleteMapping("/delete/{fin}")
+    public void deleteStudentByFin(@PathVariable String fin) {
+        studentService.deleteById(fin);
+    }
+    @ApiOperation(value = "Get Student's Orders", notes = "When you pass Student fin you will get student's Order")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully work"),
+            @ApiResponse(code = 404, message = "Invalid getting student's order by fin")
+    })
+    @GetMapping("/orders/{fin}")
+    public ResponseEntity<List<OrderResponse>> getStudentOrders(@PathVariable String fin) {
+        return ResponseEntity.ok(studentService.getStudentOrders(fin));
+    }
 }
