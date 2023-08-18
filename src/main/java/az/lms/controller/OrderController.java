758778b53e3ee327de3c06defd1708ce9c1d4b01
Mehman Osmnaov <mehmanosmanof@gmail.com>
2023-08-18 11:51:53 +0400

    Inserted @Annotations related to OpenAPI

--- src/main/java/az/lms/controller/OrderController.java
+++ src/main/java/az/lms/controller/OrderController.java
@@ -6,6 +6,7 @@ import az.lms.dto.response.OrderResponse;
 import az.lms.model.OrderType;
 import az.lms.service.OrderService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -28,10 +29,9 @@ public class OrderController {
    private final OrderService orderService;
 
 
-
-   @ApiOperation(value = "Get all orders",notes = "This method gets list of all available orders")
+   @ApiOperation(value = "Get all orders", notes = "This method gets list of all available orders")
    @ApiResponses(value = {
-           @ApiResponse(code = 200,message = "SUCCESSFUL"),
+           @ApiResponse(code = 200, message = "SUCCESSFUL"),
    })
    @GetMapping("/")
    public ResponseEntity<List<OrderResponse>> getOrders() {
@@ -44,7 +44,7 @@ public class OrderController {
            @ApiResponse(code = 400, message = "Invalid insert")
    })
    @PostMapping("/make")
-   public OrderType makeOrder(@Valid @RequestBody OrderRequest request) {
+   public OrderType makeOrder(@ApiParam(name = "request", value = "Order request object") @Valid @RequestBody OrderRequest request) {
       return orderService.createOrder(request);
    }
 
@@ -54,7 +54,7 @@ public class OrderController {
            @ApiResponse(code = 400, message = "Invalid insert")
    })
    @PostMapping("/return")
-   public OrderType returnOrder(@Valid @RequestBody OrderRequest request) {
+   public OrderType returnOrder(@ApiParam(name = "request", value = "Order request object") @Valid @RequestBody OrderRequest request) {
       return orderService.returnOrder(request);
    }
 
