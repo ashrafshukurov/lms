8382ec9ab700a6e39289908c283b0de71d61991f
Mehman Osmnaov <mehmanosmanof@gmail.com>
2023-08-24 11:29:55 +0400

    added required sql files

--- src/test/resources/sql/returns.sql
+++ src/test/resources/sql/returns.sql
@@ -1,8 +1,6 @@
 insert into orders
-values (1, 1, '2023-01-01', 'ORDERED', 1);
+values (4, 1, '2023-01-01', 'RETURNED', 2);
 insert into orders
-values (2, 1, '2023-01-02', 'RETURNED', 1);
+values (5, 1, '2023-01-02', 'ORDERED', 2);
 insert into orders
-values (3, 1, '2023-01-03', 'ORDERED', 1);
-insert into orders
-values (4, 1, '2023-01-04', 'RETURNED', 1);
+values (6, 1, '2023-01-03', 'RETURNED', 2);
