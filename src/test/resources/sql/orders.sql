43aff107e9050fe1d8d73a18bee6f4e426a5ee28
Mehman Osmnaov <mehmanosmanof@gmail.com>
2023-08-28 12:58:22 +0400

    Add WebController.java class and sql queries

--- src/test/resources/sql/orders.sql
+++
@@ -1,6 +0,0 @@
-insert into orders
-values (1, 1, '2023-01-01', 'ORDERED', 1);
-insert into orders
-values (2, 1, '2023-01-02', 'RETURNED', 1);
-insert into orders
-values (3, 1, '2023-01-03', 'ORDERED', 1);
