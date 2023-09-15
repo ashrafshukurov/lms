1fb63f07efc53fe876b070e92d08a51e3687e9a4
cahaciyev <cahaciyev@gmail.com>
2023-09-15 15:19:16 +0400

    lmsApplication changed

--- src/main/java/az/lms/LmsApplication.java
+++ src/main/java/az/lms/LmsApplication.java
@@ -3,11 +3,10 @@ package az.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebSecurity
-@EnableWebMvc
+
 public class LmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
